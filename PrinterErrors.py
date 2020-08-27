f2e452f0359a50045382daef50afc143da6c15f4
xhanak2 <51941477+xhanak2@users.noreply.github.com>
2020-08-27 11:35:21 +0200

    Update PrinterErrors.py

--- PrinterErrors.py
+++ PrinterErrors.py
@@ -1,7 +1,7 @@
 def printer_error(s):
     error=0
     for c in s:
-        if c not in '[a-m]':
+        if c not in 'abcdefghijklm':
             error+=1
     return (str(error)+"/"+str(len(s)))
 
